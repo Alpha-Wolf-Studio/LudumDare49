0d0565c3fd1169278d1cb4f97b1125fa5aa72ac2
Adrian <Owinmowe@gmail.com>
2021-10-02 01:31:33 -0300

    Added platform 3 code.

---
+++ Assets/Scripts/Platform03.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform03 : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetComponent<Player>()) 
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
