6c446094a9d9288b400bd28335227b6ace4d7655
Federico Olive <FedericoOlive@gmail.com>
2021-10-01 21:54:16 -0300

    Add UI Scripts.

---
+++ Assets/Scripts/UiButtonFx.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class UiButtonEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private float scaleMultiply = 3;
+    [SerializeField] private float limit = 1.2f;
+    private bool increment = false;
+    private Vector3 initialScale;
+    private Vector3 scale;
+
+    private void Awake()
+    {
+        increment = false;
+        initialScale = transform.localScale;
+    }
+
+    private void OnEnable()
+    {
+        transform.localScale = initialScale;
+        increment = false;
+    }
+
+    private void Update()
+    {
+        ChangeScale();
+    }
+
+    public void OnMouseEnterButton()
+    {
+        increment = true;
+        // todo: evento de mouse sobre boton ENTRA.
+    }
+    public void OnMouseExitButton()
+    {
+        increment = false;
+        // todo: evento de mouse sobre boton SALE.
+    }
+    private void ChangeScale()
+    {
+        float timeStep = scaleMultiply * Time.unscaledDeltaTime;
+        scale = transform.localScale;
+        if (increment)
+        {
+            if (transform.localScale.x < limit)
+            {
+                scale = new Vector3(scale.x + timeStep, scale.y + timeStep, scale.z + timeStep);
+                transform.localScale = scale;
+            }
+            else
+            {
+                transform.localScale = new Vector3(limit, limit, limit);
+            }
+        }
+        else
+        {
+            if (transform.localScale.x > initialScale.x)
+            {
+                scale = new Vector3(scale.x - timeStep, scale.y - timeStep, scale.z - timeStep);
+                transform.localScale = scale;
+            }
+            else
+            {
+                transform.localScale = new Vector3(initialScale.x, initialScale.y, initialScale.z);
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnMouseEnterButton();
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnMouseExitButton();
+    }
+}
