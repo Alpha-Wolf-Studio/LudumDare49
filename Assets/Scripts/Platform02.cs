9e7b450cda3ccc1ae1c1cdb4bb9fb006cfbb0e2f
Federico Olive <FedericoOlive@gmail.com>
2021-10-02 00:21:09 -0300

    Add Platform 02.

---
+++ Assets/Scripts/Platform02.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+public class Platform02 : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private float minRandom = 1.2f;
+    [SerializeField] private float MaxRandom = 3.0f;
+    [SerializeField] private float maxTimeAlive;
+    private float onTimeAlive;
+    private Color colorStart;
+    private Color colorEnd = Color.black;
+    private bool firstCollision;
+
+    private void Start()
+    {
+        if (maxTimeAlive == 0)
+        {
+            maxTimeAlive = Random.Range(minRandom, MaxRandom);
+        }
+        colorStart = image.color;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!firstCollision)
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                firstCollision = true;
+                StartDestroy();
+            }
+        }
+    }
+    IEnumerator StartDestroy()
+    {
+        while (onTimeAlive < maxTimeAlive)
+        {
+            onTimeAlive += Time.deltaTime;
+            image.color = Color.Lerp(colorStart, colorEnd, onTimeAlive / maxTimeAlive);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
