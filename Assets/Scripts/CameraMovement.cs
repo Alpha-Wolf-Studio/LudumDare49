b251b9611a0f2cfceca5485f742abfbff17f3ed3
PabloGoldman <pablo.goldmanarca@gmail.com>
2021-10-02 00:31:09 -0300

    Added platform01 script.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float incrementalSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
         transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y);
+
+        speed += incrementalSpeed;
     }
 }
+
+
---
+++ Assets/Scripts/Platform01.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class Platform01 : MonoBehaviour
+{
+    
+    private bool firstCollision;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.isKinematic = true;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!firstCollision)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                firstCollision = true;
+                rb.isKinematic = false;
+            }
+        }
+    }
+}
