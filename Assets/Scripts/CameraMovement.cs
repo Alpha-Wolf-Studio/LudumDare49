ff347bd12d5be2ea837c169f6a5664072fe243cf
Adrian <Owinmowe@gmail.com>
2021-10-02 02:13:08 -0300

    Added camera pan on player getting outside the camera.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+
+    [SerializeField] private Transform playerTransform;
+    [Space(10)]
     [SerializeField] private float speed;
     [SerializeField] private float incrementalSpeed;
+    [SerializeField] private float catchUpDistanceX;
+    [SerializeField] private float catchUpSpeedMultiplier; 
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +26,15 @@ public class CameraMovement : MonoBehaviour
 
     void Movement()
     {
-        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
-
+        float playerX = playerTransform.position.x;
+        if (Mathf.Abs(playerX - transform.position.x) > catchUpDistanceX) 
+        {
+            transform.position = new Vector3(transform.position.x + speed * catchUpSpeedMultiplier * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        else 
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
         speed += incrementalSpeed * Time.deltaTime;
     }
 }
