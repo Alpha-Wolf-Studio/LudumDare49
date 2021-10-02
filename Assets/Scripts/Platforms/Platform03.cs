f51e7f29c59c753efe379d1e97855ae62111a82e
Adrian <Owinmowe@gmail.com>
2021-10-02 14:45:24 -0300

    Added lower collider for platform 3.

--- Assets/Scripts/Platforms/Platform03.cs
+++ Assets/Scripts/Platforms/Platform03.cs
@@ -1,19 +1,40 @@
 ﻿using UnityEngine;
+using System.Collections;
 public class Platform03 : MonoBehaviour, IPlatform
 {
-    [SerializeField] PlatformBase basePlatform;
+    [Header("Pillars references")]
+    [SerializeField] PlatformBase basePlatform = null;
+    [SerializeField] GameObject leftSidePlatform = null;
+    [SerializeField] GameObject rightSidePlatform = null;
+    [SerializeField] GameObject lowPlatform = null;
+    [Space(10)]
+    [SerializeField] float timeToRemovePlatform = 1f;
+
     Rigidbody2D rb;
+    Rigidbody2D leftRB;
+    Rigidbody2D rightRB;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        leftRB = leftSidePlatform.GetComponent<Rigidbody2D>();
+        rightRB = rightSidePlatform.GetComponent<Rigidbody2D>();
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (Funcs.Get().LayerEqualPlayer(other.gameObject.layer))
         {
             rb.bodyType = RigidbodyType2D.Dynamic;
+            leftRB.bodyType = RigidbodyType2D.Dynamic;
+            rightRB.bodyType = RigidbodyType2D.Dynamic;
+            StartCoroutine(DestroyLowPlatform());
         }
+    }
+
+    IEnumerator DestroyLowPlatform() 
+    {
+        yield return new WaitForSeconds(timeToRemovePlatform);
+        lowPlatform.SetActive(false);
     }
 
     void IPlatform.DestroyBase()
