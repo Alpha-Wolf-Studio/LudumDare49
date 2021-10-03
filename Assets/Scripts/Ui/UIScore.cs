a3d95a6c032f0e35e30a45f8f4d94dcefbe0d3ae
Adrian <Owinmowe@gmail.com>
2021-10-03 19:08:22 -0300

    Added color glow on gameplay texts.

--- Assets/Scripts/Ui/UIScore.cs
+++ Assets/Scripts/Ui/UIScore.cs
@@ -6,6 +6,13 @@ public class UIScore : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] TextMeshProUGUI stageText;
+    [Header("Glowing Configuration")]
+    [SerializeField] float glowingSpeed = 1f;
+    [SerializeField] Color glowDownColor;
+    [SerializeField] Color glowUpColor;
+    bool glowingUp = true;
+    float t = 0;
+
     private int stage = 0;
     Player player;
     GameManager gameManager;
@@ -17,24 +24,43 @@ public class UIScore : MonoBehaviour
         gameManager = FindObjectOfType<GameManager>();
         gameManager.OnStageChange += ShowChangeState;
         gameManager.OnResetLevel += ResetStage;
-        stageText.text = "STAGE 0";
+        stageText.text = "STAGE - 0";
+        scoreText.color = glowDownColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = player.points.ToString();
+        if (glowingUp)
+        {
+            t += Time.deltaTime * glowingSpeed;
+            if (t > 1)
+            {
+                glowingUp = false;
+            }
+        }
+        else
+        {
+            t -= Time.deltaTime * glowingSpeed;
+            if (t < 0)
+            {
+                glowingUp = true;
+            }
+        }
+        scoreText.color = Color.Lerp(glowDownColor, glowUpColor, t);
+        stageText.color = Color.Lerp(glowDownColor, glowUpColor, t);
+        scoreText.text = "Score - " + player.points.ToString();
     }
 
     void ShowChangeState()
     {
         stage++;
-        stageText.text = "STAGE " + stage.ToString();
+        stageText.text = "STAGE - " + stage.ToString();
     }
 
     void ResetStage()
     {
         stage=0;
-        stageText.text = "STAGE " + stage.ToString();
+        stageText.text = "STAGE - " + stage.ToString();
     }
 }
