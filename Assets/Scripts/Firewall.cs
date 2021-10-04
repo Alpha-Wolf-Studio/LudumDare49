5a5c6ac8c649bd70a1c92495c779978dbca75187
Federico Olive <FedericoOlive@gmail.com>
2021-10-04 18:28:59 -0300

    INT Firewall ART.

--- Assets/Scripts/Firewall.cs
+++ Assets/Scripts/Firewall.cs
@@ -1,9 +1,31 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Firewall : MonoBehaviour
 {
+    public GameManager gm;
+    [Serializable] public class Stage
+    {
+        public Sprite[] wall;
+        public Sprite eyes;
+        public Sprite eyebrows;
+        public Sprite[] fires;
+    }
+    public List<Stage> stages = new List<Stage>();
+
+    private void Awake()
+    {
+        if (!gm)
+        {
+            Debug.LogWarning("GameManager no asignado. ", gameObject);
+            gm = FindObjectOfType<GameManager>();
+        }
+    }
+    private void Start()
+    {
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
@@ -18,5 +40,33 @@ public class Firewall : MonoBehaviour
         {
             platform.DestroyBase();
         }
+    }
+    void FirewallAngry()
+    {
+
+    }
+    void Stage00()
+    {
+
+    }
+    void Stage01()
+    {
+
+    }
+    void Stage02()
+    {
+
+    }
+    void Stage03()
+    {
+
+    }
+    void Stage04()
+    {
+
+    }
+    void Stage05()
+    {
+
     }
 }
