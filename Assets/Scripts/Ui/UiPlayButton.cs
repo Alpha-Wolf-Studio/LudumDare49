708cfe6a8d4b7e5332a65ba2db355797b7b73cbe
Federico Olive <FedericoOlive@gmail.com>
2021-10-05 11:19:48 -0300

    Update Sounds.

---
+++ Assets/Scripts/Ui/UiPlayButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class UiPlayButton : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip audioClipPlay;
+    public void PlayButton()
+    {
+        audioSource.clip = audioClipPlay;
+        audioSource.Play();
+    }
+}
